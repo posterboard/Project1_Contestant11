4c6ec46cbfd88ddd26fe5ae3e3f27068267850ab
posterboard <67428806+posterboard@users.noreply.github.com>
2022-04-09 08:39:43 -0400

    Update Conversion.cs

--- Conversion.cs
+++ Conversion.cs
@@ -1,42 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class Conversion : MonoBehaviour
 {
     private string inputString;
-    private double roundedCentimeters;
     private double preciseInches;
-    private int preciseInchInt;
     [SerializeField] private Text input;
     [SerializeField] private Text output;
-    [SerializeField] private GameObject inputObject;
+    [SerializeField] private InputField inputObject;
     public void getInput()
     {
         inputString = input.text;
     }
+    //attached to calculate button-gets value of textfield in input field, converts and rounds to nearest Integer, and displays it.
     public void displayOutput()
     {
         getInput();
         if (isDouble(inputString))
         {
-            output.text = (preciseInches*2.54).ToString();
+            output.text = (preciseInches * 2.54).ToString("F0");
         }
         else
         {
             output.text = "Invalid Input";
         }
-        
+
     }
+    //attached to exit button
     public void quitApplication()
     {
         Application.Quit();
     }
     public void clear()
     {
-        
+
+        inputObject.text = "";
         output.text = "~";
-        
+
     }
 
 
@@ -44,15 +45,15 @@ public class Conversion : MonoBehaviour
 
     public bool isDouble(string text)
     {
-        Debug.Log(text);
-       if (double.TryParse(text,out preciseInches))
+
+        if (double.TryParse(text, out preciseInches))
         {
-            Debug.Log("True");
+
             return true;
         }
-        Debug.Log("False");
+
         return false;
-      
+
     }
-   
+
 }
